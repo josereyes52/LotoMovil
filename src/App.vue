aa52dc40fe42fb355ca27f5c90e1bba17c927fdc
Jose Reyes <joseantonioreyes52@gmail.com>
2022-05-08 13:50:21 -0400

    Add Bootstrap and fontawesome library to the project

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,14 @@
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
 import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue'
+import Footer from './components/Footer.vue'
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
+  <Header />
   <HelloWorld msg="Hello Vue 3 + Vite con algo" />
+  <Footer />
 </template>
 
 <style>
@@ -16,6 +19,5 @@ import HelloWorld from './components/HelloWorld.vue'
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,30 @@
+<template>
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+              <div class="container">
+                <!-- <a class="navbar-brand" href="index.html">Logo</a> -->
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample07" aria-controls="navbarsExample07" aria-expanded="false" aria-label="Toggle navigation">
+                  <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarsExample07">
+                  <ul class="navbar-nav mr-auto">
+                    <li class="nav-item active">
+                      <a class="nav-link" href="index.html">Home<span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item dropdown">
+                      <a class="nav-link dropdown-toggle" href="#" id="dropdown07" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Other</a>
+                      <div class="dropdown-menu" aria-labelledby="dropdown07">
+                        <a class="dropdown-item" href="Result.html">Results</a>
+                        <a class="dropdown-item" href="Terms-of-Use.html">Terms of Use</a>
+                        <a class="dropdown-item" href="Privacy-Policy.html">Privacy Policy</a>
+                      </div>
+                    </li>
+                  </ul>
+                  <form class="form-inline my-2 my-md-0" _lpchecked="1">
+                    <input class="form-control" type="text" placeholder="Search" aria-label="Search">
+                  </form>
+                </div>
+              </div>
+            </nav>
+    </header>
+</template>
